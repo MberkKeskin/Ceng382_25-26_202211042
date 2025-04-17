be2937a94a3a6cbe6fdf6a07c710e373967dfb5e
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2025-04-17 22:35:57 +0300

    Delete LabProject directory

--- LabProject/Models/ClassInformationModelTable.cs
+++
@@ -1,7 +0,0 @@
-public class ClassInformationTable
-{
-    public string ClassName { get; set; } = string.Empty;
-    public int StudentCount { get; set; }
-    public string Description { get; set; } = string.Empty;
-    public int Id { get; set; } // Tabloda gösterilmeyecek ama işlem için lazım
-}
